b900da00bcf511c2c05c961abec0c498706bb8d7
ramansh18 <ramansh8055@gmail.com>
2024-07-10 12:04:43 +0530

    Adding Images instead of regular alphabets and small logic changes.

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
 const boxes = document.querySelectorAll(".box");
 const gameInfo = document.querySelector(".game-info");
 const newGamebtn = document.querySelector(".btn");
-
+const startBtn = document.querySelector(".start-btn");
+const playerOne = document.getElementById("pyr-1");
+const playerTwo = document.getElementById("pyr-2");
+const startScreen = document.querySelector(".start-screen");
+const TTT = document.querySelector(".tic-tac-toe");
+const formData = document.querySelector("form");
+const name_error = document.querySelector("#name-error");
+const name_error1 = document.querySelector("#name-error1");
 let currentPlayer;
 let gameGrid;
 const winningPos =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],
 [0,4,8],[2,4,6]];
 
+formData.addEventListener("submit", e=>{
+    e.preventDefault();
+    let ans = isValidate();
+    ans ? ClickHandler(): NewGame() ;
+});
+
+function isValidate(){
+    if(playerOne.value==="" &&  playerTwo.value===""){
+        name_error.innerText = "Name Cannot Be Empty";
+        name_error1.innerText = "Name Cannot Be Empty";
+        return false;
+    }
+    else if(playerOne.value==="" ){
+        name_error.innerText = "Name Cannot Be Empty";
+        return false;
+    }
+    else if(playerTwo.value===""){
+        name_error1.innerText = "Name Cannot Be Empty";
+        return false;
+    }
+    
+    return true;
+}
+TTT.classList.add("hidden");
+startBtn.addEventListener("click",ClickHandler);
+gameInfo.classList.add("hidden");
+
+function ClickHandler(){
+    TTT.classList.remove("hidden");
+    startScreen.classList.add("hidden");
+    gameInfo.classList.remove("hidden");
+    initGame();       
+}
+function NewGame(){
+    TTT.classList.add("hidden");
+    startScreen.classList.remove("hidden");
+    playerOne.value= "";
+    playerTwo.value = "";
+    newGamebtn.classList.remove("active");
+    gameInfo.classList.add("hidden");
+}
 function initGame(){
     currentPlayer = "X";
     gameGrid = ["","","","","","","","",""];
@@ -17,13 +65,8 @@ function initGame(){
 
             box.classList = `box box${index+1}`;
         });
- 
-    newGamebtn.classList.remove("active");
-
-    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+    gameInfo.innerText = `Current Player - ${playerOne.value}`;
 }
- 
-initGame();
 
 function swapTurn(){
     if(currentPlayer==="X"){
@@ -32,7 +75,7 @@ function swapTurn(){
     else{
         currentPlayer="X";
     }
-    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+    gameInfo.innerText = `Current Player - ${(currentPlayer==="X") ? playerOne.value : playerTwo.value}`;
 }
 
 function checkGameOver(){
@@ -60,7 +103,7 @@ function checkGameOver(){
         }
     });
     if(answer !== ""){
-        gameInfo.innerHTML = `Winner Player - ${answer}`;
+        gameInfo.innerHTML = `Winner Player - ${(answer==="X") ? playerOne.value : playerTwo.value}`;
         newGamebtn.classList.add("active");
         return;
     }
@@ -80,12 +123,17 @@ function checkGameOver(){
 
 function handleClick(index) {
     if(gameGrid[index] === "" ) {
-        boxes[index].innerText = currentPlayer;
+
+        boxes[index].innerHTML = (currentPlayer
+             === "X") ? '<img src="./assets/—Pngtree—vector cross icon_4275927.png" height="70px" width="70px" alt="X">' :
+         '<img src="./assets/—Pngtree—neon blue shining circle light_9156893.png" height="70px" width="70px" alt="O">';
+
+        //boxes[index].innerText = currentPlayer;
         gameGrid[index] = currentPlayer;
         boxes[index].style.pointerEvents = "none";
         //swap karo turn ko
         swapTurn();
-        //check koi jeet toh nahi gya
+        //check koi win toh ni gya
         checkGameOver();
     }
 }
@@ -96,4 +144,4 @@ boxes.forEach((box, index) => {
     })
 });
 
-newGamebtn.addEventListener("click", initGame);
+newGamebtn.addEventListener("click", NewGame);
